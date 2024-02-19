698804b93fa49ce3a27dbda86c66fa67ad75030c
Hui Yang <hui.yang.9045@gmail.com>
2024-02-19 15:22:44 -0500

    updated DoMath function of MyApp

--- src/main/java/demo/DoMath.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class DoMath {
-
-	public static double addTwoNums(double num1, double num2) {
-		return num1 * num2;
-	}
-
-}
---
+++ src/test/java/unit/myapp/DoMathTest.java
@@ -0,0 +1,19 @@
+package unit.myapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import myapp.DoMath;
+
+public class DoMathTest {
+
+	@Test
+	void searchTest() {
+		assertFalse(DoMath.search(5, new int[] {}), "Array is empty.");
+		assertFalse(DoMath.search(5, null), "Array is null.");
+		assertFalse(DoMath.search(5, new int[] { 7, 8 }), "Target is not in the array.");
+		assertTrue(DoMath.search(5, new int[] { 7, 5 }), "Target is in the array.");
+	}
+
+}
