87d8de46eac6e3593b141eb6b34de5b675abb5dc
Hui Yang <hui.yang.9045@gmail.com>
2024-02-22 16:07:28 -0500

    added Playground Form Demo Test

--- src/test/java/pages/playground/MainPage.java
+++ src/test/java/pages/playground/MainPage.java
@@ -1,9 +1,13 @@
 package pages.playground;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utilities.ConfigReader;
 import utilities.DriverManager;
@@ -11,32 +15,28 @@ import utilities.DriverManager;
 public class MainPage {
 
 	private static String fileName = "playground";
+	private static String playgroundURL = ConfigReader.getValue(fileName, "url");
 
 	private static WebDriver driver;
+	private static WebDriverWait wait;
 
-	@FindBy(linkText = "Simple Form Demo")
-	private static WebElement formDemoText;
+	@FindBy(linkText = "Input Form Submit")
+	private static WebElement formDemoLink;
 
 	public MainPage() {
 		driver = DriverManager.getDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		PageFactory.initElements(driver, this);
 	}
 
 	public void loadPage() {
-		driver.navigate().to(ConfigReader.getValue(fileName, "url"));
-	}
-
-	public boolean isPageLoaded() {
-		try {
-			return driver.getCurrentUrl().equals(ConfigReader.getValue(fileName, "url"));
-		} catch (Exception e) {
-			return false;
-		}
+		driver.navigate().to(playgroundURL);
+		wait.until(ExpectedConditions.urlToBe(playgroundURL));
 	}
 
 	public void toFormDemo() {
-		if (isPageLoaded())
-			formDemoText.click();
+		formDemoLink.click();
+		wait.until(ExpectedConditions.urlContains("/input-form-demo"));
 	}
 
 }
