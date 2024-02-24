9ad074cfb9232b11f39c6ecd695360490c0df6e2
Hui Yang <hui.yang.9045@gmail.com>
2024-02-24 04:26:37 -0800

    updated drivers

--- src/test/java/suites/web/LocalEdgeDriverInfoTestSuite.java
+++ src/test/java/suites/web/LocalEdgeDriverInfoTestSuite.java
@@ -2,14 +2,13 @@ package suites.web;
 
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
-
-import ui.demo.metadata.DriverDataTest;
-
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.IncludeTags;
 
+import ui.metadata.DriverDataTest;
+
 @Suite
-@SuiteDisplayName(value = "Local Edge Driver")
+@SuiteDisplayName(value = "Local Edge Driver Test")
 @SelectClasses(value = { DriverDataTest.class })
 @IncludeTags(value = { "local & edge" })
 public class LocalEdgeDriverInfoTestSuite {
---
+++ src/test/java/ui/selenium/LocatorDemo.java
@@ -0,0 +1,22 @@
+package ui.selenium;
+
+import org.openqa.selenium.By;
+
+public class LocatorDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+	void findLocator() {
+		By.id("id");
+		By.name("name");
+		By.className("class");
+		By.tagName("button");
+		By.cssSelector("input[id='password']");
+		By.linkText("click here");
+		By.partialLinkText("click");
+		By.xpath("//input[@id='password']");
+	}
+
+}
