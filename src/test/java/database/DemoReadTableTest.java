579b6aa15c8cd4597ef403a9e1920ee2a822acf7
Hui Yang <hui.yang.9045@gmail.com>
2024-03-24 20:20:27 -0400

    changed config file name

--- src/test/java/database/DemoReadTableTest.java
+++ src/test/java/database/DemoReadTableTest.java
@@ -17,47 +17,47 @@ import pojos.database.tables.Employees;
 
 public class DemoReadTableTest {
 
-	SessionFactory sessionFactory;
-	StandardServiceRegistry registry;
+    SessionFactory sessionFactory;
+    StandardServiceRegistry registry;
 
-	@Test
-	void runTest() {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+    @Test
+    void runTest() {
+	Session session = sessionFactory.openSession();
+	session.beginTransaction();
 
-		// table name is the pojo name, not the one from the database
-		List<Employees> result = session.createQuery("FROM Employees", Employees.class).list();
+	// table name is the pojo name, not the one from the database
+	List<Employees> result = session.createQuery("FROM Employees", Employees.class).list();
 
-		for (Employees employees : result) {
-			System.out.println(String.format("%d: %s %s, %s", employees.getEmployeeNumber(), employees.getFirstName(),
-					employees.getLastName(), employees.getJobTitle()));
-		}
-
-		session.getTransaction().commit();
-		session.close();
+	for (Employees employees : result) {
+	    System.out.println(String.format("%d: %s %s, %s", employees.getEmployeeNumber(), employees.getFirstName(),
+		    employees.getLastName(), employees.getJobTitle()));
 	}
 
-	@BeforeEach
-	void setUp() {
-		// configures settings from hibernate-config.xml (formerly hibernate.cfg.xml)
-		registry = new StandardServiceRegistryBuilder()
-				.configure(new File("src/test/resources/configs/hibernate-config.xml")).build();
-		try {
-			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-		} catch (Exception e) {
-			System.out.println("Failed to load settings from hibernate.cfg.xml.");
-			if (registry != null) {
-				StandardServiceRegistryBuilder.destroy(registry);
-			}
-			e.printStackTrace();
-		}
-	}
+	session.getTransaction().commit();
+	session.close();
+    }
 
-	@AfterEach
-	void tearDown() {
-		if (registry != null) {
-			StandardServiceRegistryBuilder.destroy(registry);
-		}
+    @BeforeEach
+    void setUp() {
+	// configures settings from hibernate.config.xml (formerly hibernate.cfg.xml)
+	registry = new StandardServiceRegistryBuilder()
+		.configure(new File("src/test/resources/configs/hibernate.config.xml")).build();
+	try {
+	    sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+	} catch (Exception e) {
+	    System.out.println("Failed to load settings from hibernate.cfg.xml.");
+	    if (registry != null) {
+		StandardServiceRegistryBuilder.destroy(registry);
+	    }
+	    e.printStackTrace();
 	}
+    }
+
+    @AfterEach
+    void tearDown() {
+	if (registry != null) {
+	    StandardServiceRegistryBuilder.destroy(registry);
+	}
+    }
 
 }
